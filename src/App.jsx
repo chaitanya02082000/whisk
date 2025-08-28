5d8dbdc4d1ec4263dc1ed29eb38872ad2470ce4b
chaitanya02082000 <chaitanyagithub0208@gmail.com>
2025-08-29 00:25:43 +0530

    did the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import Hero from "./components/hero";
 import "./App.css";
+import Sidebar from "./components/sidebar";
 
 function App() {
   return (
     <>
-      <Hero />
+      <div className="main">
+        <Sidebar />
+        <main className="content">
+          <Hero />
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,12 @@
+const Sidebar = () => {
+  return (
+    <>
+      <aside className="sidebar">
+        <h3>Whisk</h3>
+        <button>Add recipe</button>
+        <input placeholder="Search Recipes"></input>
+      </aside>
+    </>
+  );
+};
+export default Sidebar;
